55f3ae24475d61593a8c4c557135322f742bf5cd
Ajay Singh <ajayxsingh@gmail.com>
2014-10-21 20:32:34 -0400

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Ajay Singh on 10/21/14.
+//  Copyright (c) 2014 Ajay Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Ajay Singh on 10/21/14.
+//  Copyright (c) 2014 Ajay Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
