0ad9cb733b4d1557c1108a263ef22eaa720d3956
Ajay Singh <ajayxsingh@gmail.com>
2014-10-21 20:52:45 -0400

    Removed the storyboard and replaced it with a nib.

--- SearchViewController.h
+++ SearchViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SearchViewController.h
 //  StoreSearch
 //
 //  Created by Ajay Singh on 10/21/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SearchViewController : UIViewController
 
 @end
-
--- StoreSearch/AppDelegate.m
+++ StoreSearch/AppDelegate.m
@@ -8,6 +8,11 @@
 
 #import "AppDelegate.h"
 
+
+#import "SearchViewController.h"
+
+
+
 @interface AppDelegate ()
 
 @end
@@ -16,9 +21,21 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    self.searchViewController = [[SearchViewController alloc] initWithNibName:@"SearchViewController" bundle:nil];
+    
+    self.window.rootViewController = self.searchViewController;
+
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
+
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
